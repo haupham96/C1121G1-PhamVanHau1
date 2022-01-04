45f0d92457ed799dd8d46c08413107cfde6703f8
haupham96 <hau.pv.0596@gmail.com>
2022-01-04 10:56:51 +0700

    update BT mang

--- module2/src/ss3_array_and_method_in_java/exercise/CountOccurrencesOfChar.java
+++ module2/src/ss3_array_and_method_in_java/exercise/CountOccurrencesOfChar.java
@@ -6,7 +6,7 @@ public class CountOccurrencesOfChar {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str = "abcdaefab";
-        char input = 'a';
+        char input = scanner.next().charAt(0);
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == input) {
