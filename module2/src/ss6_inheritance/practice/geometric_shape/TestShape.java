234c63e75b5d95002abf577ec1859702f0d43303
haupham96 <hau.pv.0596@gmail.com>
2022-01-06 15:58:46 +0700

    update bai thuc hanh inheritance

---
+++ module2/src/ss6_inheritance/practice/geometric_shape/TestShape.java
@@ -0,0 +1,11 @@
+package ss6_inheritance.practice.geometric_shape;
+
+public class TestShape {
+    public static void main(String[] args) {
+        Shape shape1 = new Shape();
+        System.out.println(shape1);
+
+        Shape shape2 = new Shape("red",false);
+        System.out.println(shape2);
+    }
+}
