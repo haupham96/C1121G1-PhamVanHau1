f9ec93c8cb70123b4b0913c462982bd541547f61
haupham96 <hau.pv.0596@gmail.com>
2022-01-13 16:11:21 +0700

    update BT ss11

---
+++ module2/src/ss11_stack_queue/exercise/decimal_to_binary/Main.java
@@ -0,0 +1,26 @@
+package ss11_stack_queue.exercise.decimal_to_binary;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        int binary = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Binary number");
+        int decimal = Integer.parseInt(scanner.nextLine());
+        
+        do {
+            stack.push(decimal%2);
+            decimal = decimal / 2;
+
+        } while (decimal > 0);
+
+        System.out.print("Binary code = ");
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop());
+        }
+
+    }
+}
