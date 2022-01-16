281ee2da6133c5b1e50ec57f9f8f95d68ccde341
haupham96 <hau.pv.0596@gmail.com>
2022-01-16 10:18:21 +0700

    update thuc hanh ss13

---
+++ module2/src/ss13_searching_algorithms/practice/algorithm_complexsity_calculate/AlgorithmComplexityTest.java
@@ -0,0 +1,26 @@
+package ss13_searching_algorithms.practice.algorithm_complexsity_calculate;
+
+import java.util.Scanner;
+
+public class AlgorithmComplexityTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter sting : ");
+        String inputString = scanner.nextLine();
+        int[] freequenceChar = new int[255];
+        for (int i = 0; i < inputString.length(); i++) {
+            int ascii = (int) inputString.charAt(i);
+            freequenceChar[ascii] += 1;
+        }
+        int max = 0;
+        char character = (char) 255;
+        for (int i = 0; i < 255; i++) {
+            if (freequenceChar[i] > max) {
+                max = freequenceChar[i];
+                character =(char) i;
+            }
+        }
+        System.out.println("most appearing : "+character+" , which is "+max+" times ");
+
+    }
+}
