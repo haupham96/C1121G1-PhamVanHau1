1a0ba89a8e125326b7c4de720fd1e8b98a8b7f96
haupham96 <hau.pv.0596@gmail.com>
2022-01-17 17:07:28 +0700

    update thuc hanh ss14

---
+++ module2/src/ss14_sort_algorithms/practise/BubbleSortByStep.java
@@ -0,0 +1,46 @@
+package ss14_sort_algorithms.practise;
+
+import java.util.Scanner;
+
+public class BubbleSortByStep {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter list size:");
+        int size = scanner.nextInt();
+        int[] list = new int[size];
+        System.out.println("Enter " + list.length + " values:");
+        for (int i = 0; i < list.length; i++) {
+            list[i] = scanner.nextInt();
+        }
+        System.out.print("Your input list: ");
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + "\t");
+        }
+        System.out.println("\nBegin sort processing...");
+
+        bubbleSortByStep(list);
+    }
+
+    public static void bubbleSortByStep(int[] list) {
+        boolean check = true;
+        for (int i = 1; i < list.length && check; i++) {
+            check = false;
+            for (int j = 0; j < list.length - i; j++) {
+                if (list[j] > list[j + 1]) {
+                    int temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                    check = true;
+                }
+            }
+            if (!check) {
+                System.out.println("no need to sort");
+                break;
+            }
+        }
+        for (int element : list) {
+            System.out.print(element + " , ");
+        }
+        System.out.println();
+    }
+}
---
+++ module2/src/ss14_sort_algorithms/practise/SettingBubbleSort.java
@@ -0,0 +1,27 @@
+package ss14_sort_algorithms.practise;
+
+public class SettingBubbleSort {
+    static int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+
+    public static void bubbleSort(int[] list) {
+        boolean check = true;
+        for (int i = 1; i < list.length && check; i++) {
+            check = false;
+            for (int j = 0; j < list.length - i; j++) {
+                if (list[j] > list[j + 1]) {
+                    int temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                    check = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        bubbleSort(list);
+        for(int element:list){
+            System.out.print(element+" , ");
+        }
+    }
+}
