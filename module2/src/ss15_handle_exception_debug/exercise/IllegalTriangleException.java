fb4353bfeebbc1b9c6e490f5aadcbe066b044020
haupham96 <hau.pv.0596@gmail.com>
2022-01-20 08:40:12 +0700

    update BT exception

--- module2/src/ss15_handle_exception_debug/exercise/IllegalTriangleException.java
+++ module2/src/ss15_handle_exception_debug/exercise/IllegalTriangleException.java
@@ -4,30 +4,38 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class IllegalTriangleException {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("side a :");
-        int sideA = scanner.nextInt();
-
-        System.out.println("side b :");
-        int sideB = scanner.nextInt();
-
-        System.out.println("side c :");
-        int sideC = scanner.nextInt();
-
+    public static void triangleException(int sideA, int sideB, int sideC) throws Exception {
         if (sideA < 0 || sideB < 0 || sideC < 0 ||
                 sideA + sideB < sideC ||
                 sideB + sideC < sideA ||
                 sideA + sideC < sideB) {
-
-            throw new InputMismatchException("Invalid Input: " + " A= " + sideA + " , B = " + sideB + " , C = " + sideC);
+            throw new Exception();
         }
         System.out.println("Triangle");
         System.out.println("A = " + sideA);
         System.out.println("B = " + sideB);
         System.out.println("C = " + sideC);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+
+            System.out.println("side a :");
+            int sideA = scanner.nextInt();
+
+            System.out.println("side b :");
+            int sideB = scanner.nextInt();
+
+            System.out.println("side c :");
+            int sideC = scanner.nextInt();
+            IllegalTriangleException.triangleException(sideA,sideB,sideC);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println("END");
 
 
     }
-
 }
